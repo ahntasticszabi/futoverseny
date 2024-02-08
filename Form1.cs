fdde3602b3e38de1de240070f04a8da0ddec5078
Szabolcs Szabadi <creppy201@gmail.com>
2024-02-08 12:06:45 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace futoverseny
+{
+    public partial class Form1 : Form
+    {
+        public static string fajlnev {get; set;}
+        public List<Futo> lista = new List<Futo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuKilep_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuMegnyit_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                btnAdatbe.Enabled = true;
+                fajlnev = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnAdatbe_Click(object sender, EventArgs e)
+        {
+            mnuEredmenyLista.Enabled = true;
+            //var sr = new StreamReader(fajlnev);
+            var sr = new StreamReader("futok.txt");
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Futo(sr.ReadLine()));
+            }
+            sr.Close();
+
+            foreach (var sor in lista)
+            {
+                listBox1.Items.Add(sor.Nev);
+            }
+            btnAdatbe.Enabled = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtRajtszam.Text = lista[listBox1.SelectedIndex].Rajtszam;
+            txtOrszag.Text = lista[listBox1.SelectedIndex].Orszag;
+            txtIdoeredmeny.Text = lista[listBox1.SelectedIndex].Eredmeny;
+            //txtEletkor.Text = (DateTime.Today.Year - lista[listBox1.SelectedIndex].Szulido.Year).ToString();
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int dob = int.Parse(lista[listBox1.SelectedIndex].Szulido.ToString("yyyyMMdd"));
+            int age = (now - dob) / 10000;
+            txtEletkor.Text = age.ToString();
+        }
+
+        private void mnuEredmenyLista_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Eredmenylista eredmenylista = new Eredmenylista();
+            eredmenylista.ShowDialog();
+            eredmenylista.Dispose();
+            this.Show();
+        }
+    }
+}
---
+++ Futo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace futoverseny
+{
+    public class Futo
+    {
+        public string Rajtszam { get; set; }
+        public string Nev { get; set; }
+        public DateTime Szulido { get; set; }
+        public string Orszag {  get; set; }
+        public string Eredmeny { get; set; }
+
+        public Futo (string sor)
+        {
+            string[] s = sor.Split(';');
+            Rajtszam = s[0];
+            Nev = s[1];
+            Szulido = DateTime.Parse(s[2]);
+            Orszag = s[3];
+            Eredmeny = s[4];
+        }
+    }
+}
